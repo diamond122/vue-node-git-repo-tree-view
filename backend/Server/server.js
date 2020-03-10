c0962946404cb1cfeab01250ac0734e6b57c4a96
igorPae <igor.paevshik@gmail.com>
2020-03-10 11:38:34 +0200

    deploy

--- backend/Server/server.js
+++ backend/Server/server.js
@@ -80,6 +80,4 @@ app.get(
 // console.log("Listening on port " + port);
 
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
-})
+app.listen(process.env.PORT || 5000);
